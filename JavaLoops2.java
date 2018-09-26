799e27313944c1164d209a4bd706aa8232c1d6b3
Lauri Kyttälä <lauri.kyttala@gmail.com>
2018-09-26 14:52:01 +0300

    minot chages to JavaLoop2 now passing hacker validation

--- JavaLoops2.java
+++ JavaLoops2.java
@@ -8,19 +8,13 @@ class JavaLoops2 {
     public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
         List<Integer> list = new ArrayList<Integer>();
+        List<Integer> listSums = new ArrayList<Integer>();
         int t = in.nextInt();
         System.out.println("");
         for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
-
-//            System.out.print(a + " ");
-//            System.out.print("");
-//            System.out.print(b + " ");
-//            System.out.print("");
-//            System.out.print(n + " ");
-//            System.out.println("");
 
             list.add(a);
             list.add(b);
@@ -29,21 +23,19 @@ class JavaLoops2 {
         }
         in.close();
 
-//        System.out.println("list: " + list.toString());
-
         for (int i = 0, k = 0; i < t; i++) {
-            
-//            System.out.println("lakenta: " + i);
-            
-            for (int j = 0, countedNumber = 0, sum = list.get(i + k), b = list.get(i + (k+1)); j < list.get(i + (k+2)); j++) {
+
+            for (int j = 0, countedNumber = 0, sum = list.get(i + k), b = list.get(i + (k + 1)); j < list.get(i + (k + 2)); j++) {
                 countedNumber = (int) Math.pow(2, j) * b;
                 sum += countedNumber;
-                System.out.print(sum + " ");
+                listSums.add(sum);
             }
-            System.out.println("");
             k += 2;
-
+            for (Integer num : listSums) {
+                System.out.print(num + " ");
+            }
+            listSums.clear();
+            System.out.println("");
         }
-
     }
 }

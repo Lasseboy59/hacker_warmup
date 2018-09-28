3f7e21b5bfa3eba76c6b77dfef3c29c62cdc738e
Lauri Kyttala <lauri.kyttala@gmail.com>
2018-09-28 22:06:12 +0300

    ReadFile2

---
+++ ReadFile2.java
@@ -0,0 +1,29 @@
+
+
+package hacker_warmup;
+import java.io.*;
+
+public class ReadFile2 {
+
+   public static void main(String args[]) throws IOException {  
+      FileInputStream in = null;
+      FileOutputStream out = null;
+
+      try {
+         in = new FileInputStream("c:\\temp\\input.txt");
+         out = new FileOutputStream("c:\\temp\\output.txt");
+         
+         int c;
+         while ((c = in.read()) != -1) {
+            out.write(c);
+         }
+      }finally {
+         if (in != null) {
+            in.close();
+         }
+         if (out != null) {
+            out.close();
+         }
+      }
+   }
+}
